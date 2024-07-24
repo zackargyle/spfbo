5cc689b217ec8be65b571175192c31af22e68f2c
Zack Argyle <zack.argyle@gmail.com>
2024-07-24 11:29:14 -0700

    Initial site push

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,115 @@
+'use client';
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { list } from "@vercel/blob";
+
+type Store = {
+  [batch: string]: {
+    blogs: Array<string>,
+    books: BookList,
+  }
+};
+
+type Book = {
+  batch: number,
+  title: string,
+  author: string,
+  cover: string,
+  amazon: string,
+  audiobook: boolean,
+  blog: string,
+  isFinalist: boolean,
+  isSemiFinalist: boolean,
+  isCut: boolean,
+  isCoverFinalist: boolean,
+  isKU: boolean,
+};
+
+type BookList = Array<Book>;
+
+function massageData(books: BookList): Store {
+  const store: Store = {};
+
+  books.forEach((book: Book) => {
+    store[book.batch] = store[book.batch] || { blogs: [], books: [] };
+    let batch = store[book.batch];
+
+    batch.books.push(book);
+
+    if (!batch.blogs.includes(book.blog)) {
+      batch.blogs.push(book.blog);
+    }
+  });
+
+  // sort books and blogs for consistency
+  Object.keys(store).forEach(batch => {
+    store[batch].blogs.sort();
+    store[batch].books.sort((a, b) => {
+      if (a.isFinalist) return -1;
+      if (b.isFinalist) return 1;
+      if (a.isSemiFinalist) return -1;
+      if (b.isSemiFinalist) return 1;
+      if (a.isCoverFinalist) return -1;
+      if (b.isCoverFinalist) return 1;
+      return 0;
+    });
+  });
+
+  return store;
+}
+
 
 export default function Home() {
+  const [store, setStore] = useState<Store>();
+  const [selectedBatch, setSelectedBatch] = useState<string>('10');
+
+  useEffect(() => {
+    fetch("https://sof81sccysblftg6.public.blob.vercel-storage.com/spfbo-7-24-iu4AOoNBEnUz6cgIdtkZElrANnJpWp.json")
+      .then(response => response.json())
+      .then((bookList: BookList) => massageData(bookList))
+      .then((store: Store) => setStore(store));
+  },[]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+      <div>
+        <a style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+          <img src="https://cdn.prod.website-files.com/5ecfe69e4752d3ccbbe99d9d/6642b79fc3b65d5acfcb62c6_x.png" sizes="(max-width: 640px) 100vw, 640px" srcSet="https://cdn.prod.website-files.com/5ecfe69e4752d3ccbbe99d9d/6642b79fc3b65d5acfcb62c6_x-p-500.png 500w, https://cdn.prod.website-files.com/5ecfe69e4752d3ccbbe99d9d/6642b79fc3b65d5acfcb62c6_x.png 640w" alt="Self-Published Fantasy Blog-off" />
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {store ? (
+          <>
+            <div style={{ display: 'flex', width: '100%', marginTop: 20, justifyContent: 'center'}}>
+              {Object.keys(store).map(batch => (
+                <button key={batch} onClick={() => setSelectedBatch(batch)} style={{ fontSize: 20, margin: '0 15px', color: selectedBatch === batch ? '#ed1c25' : 'white'}}>{`SPFBO ${batch}`}</button>
+              ))}
+            </div>
+            <div role="list" style={{width: '90vw', marginLeft: '10vw', marginTop: 60, overflow: 'auto'}}>
+              {store[selectedBatch].blogs.map(blog => (
+                <div role="listitem" key={blog}>
+                  <h2 style={{ marginLeft: '5vw', fontSize: 40, marginBottom: 10 }}>{blog}</h2>
+                  <div role="list" style={{ flexDirection: 'row', alignItems: 'flex-start', height: 230, display: 'flex', overflow: 'auto' }}>
+                    {store[selectedBatch].books.filter(book => book.blog === blog).map((book: Book, index: number) => (
+                      <div role="listitem" key={book.title} style={{ height: 180, marginRight: 15, textAlign: 'center', fontSize: 14}}>
+                        <a href={book.amazon} target="_blank" style={{ display: 'inline-block', width: 120, position: 'relative'}}>
+                          {book.isFinalist ? (
+                            <div style={{border: '4px solid #56ca56', borderRadius: 4, width: '100%', height: '100%', position: 'absolute', top: 0, left: 0}}></div>
+                          ) : null}
+                          <img loading={index < 10 ? 'eager' : 'lazy'} alt={book.title} src={book.cover} style={{borderRadius: 4, minWidth: '100%', height: 180}} />
+                        </a>
+                        {book.isFinalist ? (
+                          <div>Finalist</div>
+                        ) : null}
+                        {book.isSemiFinalist && !book.isFinalist? (
+                          <div >Semi-finalist</div>
+                        ) : null}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : null}
       </div>
     </main>
   );
