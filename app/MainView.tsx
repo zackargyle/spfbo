7ae8cf141732ae4ad9751d9a5084e51e9ac4106a
Zack Argyle <zackargyle@fb.com>
2024-08-26 09:52:06 -0700

    Make Main easier to read

--- app/MainView.tsx
+++ app/MainView.tsx
@@ -18,48 +18,73 @@ type TLayout = {
 export default function Main(props: TProps) {
   const store = useMemo<TStore>(createStore, []);
 
-  let view: TLayout = useMemo(() => {
-    if (props.selectedView === 'finalists') {
-      return {
-        batchList: Object.keys(store).filter(batch => store[batch].finalists.length),
-        getBooks: (batch: string) => store[batch].finalists.filter((book: TBook) => bookFilter(book, book.blog, props.selectedFilter)),
-        highlight: ['winner'],
-      };
-    } else {
-      return {
-        batchList: store[props.selectedView].blogs,
-        getBooks: (batch: string) => store[props.selectedView].books.filter((book: TBook) => bookFilter(book, batch, props.selectedFilter)),
-        highlight: ['winner', 'finalist'],
-      };
-    }
-  }, [props.selectedView, props.selectedFilter]);
+  if (props.selectedView === 'finalists') {
+    return (
+      <BatchList data={Object.keys(store).filter(batch => store[batch].finalists.length > 0)}>
+        {(batch: string) =>
+          <BookList
+            books={store[batch].finalists.filter(bookFilter(props.selectedFilter))}
+            highlight={['winner']}
+          />
+        }
+      </BatchList>
+    );
+  } else {
+    return (
+      <BatchList data={Object.keys(store[props.selectedView].blogs)}>
+        {(blog) =>
+          <BookList
+            books={store[props.selectedView].blogs[blog].filter(bookFilter(props.selectedFilter))}
+            highlight={['winner', 'finalist']}
+          />
+        }
+      </BatchList>
+    );
+  }
+}
 
+type TBatchListProps = {
+  data: Array<string>,
+  children: (batch: string) => JSX.Element,
+};
+
+function BatchList({data, children}: TBatchListProps) {
   return (
     <div role="list" className="mt-12 overflow-auto" style={{width: '90vw', marginLeft: '10vw'}}>
-      {view.batchList.map(batch => (
+      {data.map(batch => (
         <div role="listitem" key={batch}>
           <h2 className="ml-8 mb-4 text-4xl">{batch.toUpperCase()}</h2>
-          <div role="list" className="flex flex-row items-start overflow-auto">
-            {view.getBooks(batch)
-              .map((book: TBook) => <BookView key={book.title} book={book} highlight={view.highlight} />)
-            }
-          </div>
+          {children(batch)}
         </div>
       ))}
     </div>
   );
 }
 
-function bookFilter(book: TBook, blog: string, filter: TFilter): boolean {
-  if (book.blog !== blog) {
-      return false;
-  }
-  switch (filter) {
-    case 'all':
-      return true;
-    case 'audio':
-      return book.audiobook;
-    case 'ku':
-      return book.isKU;
-  }
+type TBookListProps = {
+  books: TBookList,
+  highlight: Array<TBook['status']>,
+};
+
+function BookList({books, highlight}: TBookListProps) {
+  return (
+    <div role="list" className="flex flex-row items-start overflow-auto">
+      {books.map((book: TBook) => <BookView key={book.title} book={book} highlight={highlight} />)}
+    </div>
+  );
+}
+
+function bookFilter( filter: TFilter) {
+  return (book: TBook) =>  {
+    switch (filter) {
+      case 'all':
+        return true;
+      case 'ku':
+        return book.isKU;
+      case 'audio':
+        return book.audiobook;
+      default:
+        return true;
+    }
+  };
 }
--- app/store.tsx
+++ app/store.tsx
@@ -20,8 +20,9 @@ export type TBookList = Array<TBook>;
 
 export type TStore = {
   [batch: string]: {
-      blogs: Array<string>,
-      books: TBookList,
+      blogs: {
+        [blog: string]: TBookList,
+      },
       finalists: TBookList,
   },
 };
@@ -36,39 +37,40 @@ function createStore(books: TBookList): TStore {
 
   books.forEach((book: TBook) => {
     const batchKey = 'spfbo-' + book.batch;
-    store[batchKey] = store[batchKey] || { blogs: [], books: [], finalists: [] };
+    store[batchKey] = store[batchKey] || { blogs: {}, finalists: [] };
     let batch = store[batchKey];
-
-    batch.books.push(book);
 
     if (book.status === 'winner' || book.status === 'finalist') {
       batch.finalists.push(book);
     }
 
-    if (!batch.blogs.includes(book.blog)) {
-      batch.blogs.push(book.blog);
+    if (!batch.blogs[book.blog]) {
+      batch.blogs[book.blog] = [];
     }
+    batch.blogs[book.blog].push(book);
   });
 
   // sort books and blogs for consistency
-  Object.keys(store).forEach(batch => {
-    store[batch].blogs.sort();
+  Object.keys(store).forEach(batchKey => {
+    const batch = store[batchKey];
+    Object.keys(batch.blogs).forEach(blog => {
+      const bookList = batch.blogs[blog];
+      // Sort for default view
+      bookList.sort((book1, book2) => {
+        if (book1.status === 'winner') return -1;
+        if (book2.status === 'winner') return 1;
+        if (book1.status === 'finalist') return -1;
+        if (book2.status === 'finalist') return 1;
+        if (book1.status === 'semi-finalist') return -1;
+        if (book2.status === 'semi-finalist') return 1;
+        if (book1.status === 'cut') return 1;
+        if (book2.status === 'cut') return -1;
+        return 0;
+      });
 
-    // Sort for default view
-    store[batch].books.sort((book1, book2) => {
-      if (book1.status === 'winner') return -1;
-      if (book2.status === 'winner') return 1;
-      if (book1.status === 'finalist') return -1;
-      if (book2.status === 'finalist') return 1;
-      if (book1.status === 'semi-finalist') return -1;
-      if (book2.status === 'semi-finalist') return 1;
-      if (book1.status === 'cut') return 1;
-      if (book2.status === 'cut') return -1;
-      return 0;
-    });
-
-    store[batch].finalists.sort((book1, book2) => {
-      return book2.rank > book1.rank ? -1 : 1;
+      batch.finalists.sort((book1, book2) => {
+        return book2.rank > book1.rank ? -1 : 1;
+      });
     });
   });
 
